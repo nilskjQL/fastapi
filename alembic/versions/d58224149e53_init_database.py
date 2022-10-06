27af82840291f3325f2758c681de82235e9b4f38
nilskj <nilskjellman@gmail.com>
2022-10-06 21:23:01 +0200

    add db

---
+++ alembic/versions/d58224149e53_init_database.py
@@ -0,0 +1,24 @@
+"""init database
+
+Revision ID: d58224149e53
+Revises: 
+Create Date: 2022-10-06 21:13:53.335637
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd58224149e53'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
